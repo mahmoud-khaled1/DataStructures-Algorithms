a95e1faea7c5230d8a13cf85aea0afb8e7e5eb77
Mahmoud <43557035+mahmoud-khaled1@users.noreply.github.com>
2021-10-04 15:14:15 +0200

    Selection Sort

---
+++ Grokking Algorithms book/02-Selection Sort/Selection Sort/Selection Sort/Selection Sort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        public static void SelectionSort(int[]arr)
+        {
+            int min_idx;
+            for (int i=0;i<arr.Length-1;i++)
+            {
+                min_idx = i;
+                for (int k=i+1;k<arr.Length;k++)
+                {
+                    if(arr[k]<arr[min_idx])
+                    {
+                        min_idx = k;
+                    }
+                }
+                // Swap the found minimum element with the first element
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+
+            //Print Array after Sorting it 
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 55, 66, 8, 71, 0, 11, 415 };
+
+            SelectionSort(arr);
+
+        }
+    }
+}
