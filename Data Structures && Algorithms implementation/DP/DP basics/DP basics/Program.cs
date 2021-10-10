f893944b0e31c75c0147cef23da818c327c94159
Mahmoud <43557035+mahmoud-khaled1@users.noreply.github.com>
2021-10-10 23:31:17 +0200

    add

---
+++ Data Structures && Algorithms implementation/DP/DP basics/DP basics/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_basics
+{
+    class Program
+    {
+        public static int[] arr = new int[100];
+        public static void IniArray()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = -1;
+            }
+        }
+        public static int fibSave(int num)
+        {
+            if (num <= 1)
+                return 1;
+            if (arr[num] != -1)
+                return arr[num];
+            return arr[num] = fibSave(num - 2) + fibSave(num - 1);
+        }
+
+        public static int Fib(int n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+                return Fib(n - 2) + Fib(n - 1);
+        }
+        private static int  bottomUp_Dp_Fib_(int n)
+        {
+            int[] fib = new int[105];
+            fib[0] = 1;     // base case
+            fib[1] = 1;    // base case
+
+            for (int i = 2; i <= 100; ++i)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+              
+            }// bottom up according to recurrence
+
+            return fib[n];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(5)); // O(2^n)
+
+            // Top-Down approch (Memoization)
+            IniArray();
+            Console.WriteLine(fibSave(5)); // O(n)
+
+            // bottom up (Dp)
+            Console.WriteLine(bottomUp_Dp_Fib_(5));
+
+        }
+
+        
+    }
+}
---
+++ Data Structures && Algorithms implementation/DP/DP basics/Grid Problem/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid_Problem
+{
+    class Program
+    {
+        public static int n;
+        public static int[,] dp = new int[n+3,n+3];
+        public static int[,] grid = new int[n + 3, n + 3];
+        public static void InitDp()
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    dp[i, j] = -1;
+                }
+            }
+        }
+        public static void InitArray()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    grid[i, j] =int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public static bool Valid(int r,int c)
+        {
+            if (r < n && c < n)
+                return true;
+            else
+                return false;
+        }
+        public static int MaxPath_Sum(int r,int c)
+        {
+            //1- Always handle invalid calls
+            if (!Valid(r, c))
+                return 0;
+
+            //2-Handle Base Case
+            if (r == n - 1 && c == n - 1)
+                return grid[r, c];
+
+            //Check if visited before
+            if (dp[r, c] != -1)
+                return dp[r, c];
+
+            int path1 = MaxPath_Sum(r, c + 1); //right
+            int path2 = MaxPath_Sum(r + 1, c); // down 
+
+            return dp[r, c] = grid[r, c] + Math.Max(path1, path2);
+            
+        }
+        static void Main(string[] args)
+        {
+            // Given grid of positive numbers, Start from 0,0
+            // and end at n, n Move only to right and down -
+            // find path with sum of numbers is maximum.
+            //-----------------------------------------------
+
+            // How to trun code to memization?
+            // 1- Create array of input dimensions, and output of its return.
+            // E.g. int mem[MAX][MAX];
+            // Initialize it with a value that will never be a correct answer, e.g. -1
+            // If value is -1, then it is not visited before. Else, use saved value
+            //-------------------------------------------------
+
+            n = 3;
+            InitDp();
+            InitArray();
+            Console.WriteLine(MaxPath_Sum(0,0));
+
+        }
+    }
+}
