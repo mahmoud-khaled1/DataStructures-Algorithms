ee33a4f9433a8b0078c6d5fc3efc199ad90352d3
Mahmoud <43557035+mahmoud-khaled1@users.noreply.github.com>
2021-10-11 14:06:53 +0200

    Hash Table

---
+++ Grokking Algorithms book/05-hash tables/HashTables/HashTables/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // price of groceries Problem 
+
+            // Creation of Hash Table 
+            Dictionary<string, decimal> Menu = new Dictionary<string, decimal>();
+
+            // Insert into Hash Table
+            Menu.Add("apple", 0.67m);
+            Menu.Add("milk", 1.49m);
+            Menu.Add("avocado", 1.49m);
+
+            //Loop Through the hash table 
+
+            foreach (var item in Menu)
+            {
+                Console.WriteLine(item.Key+" : "+item.Value);
+            }
+
+            Console.WriteLine("-------------------------");
+
+            //Check Voter Problem
+
+            Dictionary<string, bool> Voted = new Dictionary<string, bool>();
+
+            Voted.Add("mahmoud", true);
+            Voted.Add("Ali", false);
+            Voted.Add("Asd", true);
+
+            if(Voted.ContainsKey("Ahmed"))
+            {
+                Console.WriteLine("Kick him out !!!");
+            }
+            else
+            {
+                Voted.Add("Ahmed", true);
+                Console.WriteLine("Let him Vote.");
+            }
+        }
+    }
+}
